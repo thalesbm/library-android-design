150406655a5c93cb3afe2992733387347e96a22f
Thales Marega <thales.bm92@gmail.com>
2020-08-21 19:06:17 -0300

    added new fonts

---
+++ library/src/main/java/bm/it/mobile/ui/fonts/TextViewMedium.kt
@@ -0,0 +1,24 @@
+package bm.it.mobile.ui.fonts
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class TextViewMedium : AppCompatTextView {
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0) {
+        init()
+    }
+
+    private fun init() {
+        typeface = Typeface.createFromAsset(context.assets, "fonts/roboto_medium.ttf")
+    }
+}
