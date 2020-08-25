91468b8b084bdc4b79fa2839dd1d57beff43e542
Thales Marega <thales.bm92@gmail.com>
2020-08-24 22:52:55 -0300

    added IActivity

--- library/src/main/java/bm/it/mobile/ui/view/BaseActivity.kt
+++ library/src/main/java/bm/it/mobile/ui/view/BaseActivity.kt
@@ -2,19 +2,30 @@ package bm.it.mobile.ui.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 
-abstract class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), IActivity {
 
     abstract fun initView()
-    abstract fun initExtras()
+
     abstract fun getContentView(): Int
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initExtras()
+
+        callBeforeLoadXML()
+
         setContentView(getContentView())
 
         initView()
-        initExtras()
+    }
+
+    override fun callBeforeLoadXML(){
+        // not used here
+    }
+
+    override fun initExtras() {
+        // not used here
     }
 }
---
+++ library/src/main/java/bm/it/mobile/ui/view/IActivity.kt
@@ -0,0 +1,8 @@
+package bm.it.mobile.ui.view
+
+interface IActivity {
+
+    fun callBeforeLoadXML()
+
+    fun initExtras()
+}
