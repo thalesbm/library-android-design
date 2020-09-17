8d04c1fd6d97e526aa7326e67b44bad5f0f90293
Thales Marega <thales.bm92@gmail.com>
2020-09-16 22:25:04 -0300

    adicionado helpers

--- library/src/main/java/bm/it/mobile/ui/helpers/ViewHelpers.kt
+++ library/src/main/java/bm/it/mobile/ui/helpers/ViewHelpers.kt
@@ -5,7 +5,7 @@ import android.util.DisplayMetrics
 
 object ViewHelpers {
 
-    fun calculateNoOfColumns(context: Context, columnSize: Int): Int {
+    fun calculateNoOfColumns(context: Context, columnSize: Float): Int {
         val displayMetrics: DisplayMetrics = context.resources.displayMetrics
         val screenWidthDp = displayMetrics.widthPixels / displayMetrics.density
         return (screenWidthDp / columnSize + 0.5).toInt()
